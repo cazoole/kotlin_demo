9755141efc4635c0d76dfb0796c3eab1cc546a94
cazoole <32027160+cazoole@users.noreply.github.com>
2022-11-25 15:32:11 +0800

    Initial commit

---
+++ src/test/kotlin/com/lw/kotlin_demo/KotlinDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lw.kotlin_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
